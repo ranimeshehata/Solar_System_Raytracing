78f8e74e059fe5c64e98c1ba7760d0f1d777da35
nouranKhalil <nouran.ahraf.youssef@gmail.com>
2025-05-13 05:07:17 +0300

    Added Sphere Documentation

--- src/objects/sphere.py
+++ src/objects/sphere.py
@@ -3,12 +3,38 @@ import math
 
 
 class Sphere:
-    def __init__(self, r, sectors=50, stacks=50):
+    """
+    A class to generate the geometry of a 3D sphere including vertex positions,
+    texture coordinates, and indices for rendering using triangle and line primitives.
+
+    Attributes:
+        r (float): Radius of the sphere.
+        sectors (int): Number of longitudinal slices (vertical divisions).
+        stacks (int): Number of latitudinal slices (horizontal divisions).
+    """
+
+    def __init__(self, r, sectors=36, stacks=18):
+        """
+        Initialize the sphere with given radius and resolution.
+
+        Args:
+            r (float): Radius of the sphere.
+            sectors (int): Number of vertical subdivisions (default: 36).
+            stacks (int): Number of horizontal subdivisions (default: 18).
+        """
         self.r = r
         self.sectors = sectors
         self.stacks = stacks
 
     def build_sphere_points(self):
+        """
+        Generate vertex positions and texture coordinates for the sphere.
+
+        Returns:
+            tuple:
+                - xy_coords (list of float): Flattened list of 3D vertex positions [x, y, z].
+                - tex_coords (list of float): Flattened list of 2D texture coordinates [u, v].
+        """
         xy_coords, tex_coords = [], []
 
         theta_step = 2 * np.pi / self.sectors
@@ -26,17 +52,39 @@ class Sphere:
 
                 xy_coords.extend([x, y, z])
                 tex_coords.extend([j / self.sectors, i / self.stacks])
+
         return xy_coords, tex_coords
 
     def combine_coordinates(self, xy_coords, tex_coords):
+        """
+        Combine vertex positions and texture coordinates into a single buffer.
+
+        Args:
+            xy_coords (list of float): Flattened list of 3D vertex positions.
+            tex_coords (list of float): Flattened list of 2D texture coordinates.
+
+        Returns:
+            np.ndarray: Flattened array of combined vertex attributes [x, y, z, u, v].
+        """
         xy_coords = np.asarray(xy_coords, dtype=np.float32).reshape(-1, 3)
         tex_coords = np.asarray(tex_coords, dtype=np.float32).reshape(-1, 2)
-        vertex_data = np.column_stack((xy_coords, tex_coords)).flatten().astype(np.float32)
+        vertex_data = (
+            np.column_stack((xy_coords, tex_coords)).flatten().astype(np.float32)
+        )
         return vertex_data
 
     def build_indices(self):
+        """
+        Build index buffers for rendering the sphere using triangles and lines.
+
+        Returns:
+            tuple:
+                - indices (list of int): Indices for rendering triangles.
+                - lineIndices (list of int): Indices for rendering wireframe lines.
+        """
         k1, k2 = 0, 0
         indices, lineIndices = [], []
+
         for i in range(self.stacks):
             k1 = i * (self.sectors + 1)
             k2 = k1 + self.sectors + 1
@@ -54,9 +102,12 @@ class Sphere:
 
                 lineIndices.append(k1)
                 lineIndices.append(k2)
+
                 if i != 0:
                     lineIndices.append(k1)
                     lineIndices.append(k1 + 1)
+
                 k1 += 1
                 k2 += 1
+
         return indices, lineIndices
